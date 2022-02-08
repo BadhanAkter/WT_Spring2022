0c5b8a1dd1bdf105e339d2c6e327870c0a177621
BadhanAkter <98724609+BadhanAkter@users.noreply.github.com>
2022-02-08 21:12:36 +0600

    Create Task1.php

---
+++ Task1.php/Task1.php
@@ -0,0 +1,50 @@
+# WT_Spring2022
+<html>
+<body>
+
+<h1> Registration Form </h1>
+<form action+"/action_page.php">
+    <table>
+<tr>
+<td> First Name: </td>
+<td> <input type="text" first name+"first name" placeholder="Enter your first name"> </td>
+</tr>
+<tr>
+<td> Last Name: </td>
+<td> <input type="text" last name="last name" placeholder="Enter your last name"   </td>
+</tr>
+<tr>
+ <td> Age: </td>
+ <input type="number">   
+</tr>
+<tr>
+<td> Designation</td>
+<td> 
+<input type="radio" name="designation"> Junior Programmer
+<input type="radio" name="designation"> Senior Programmer
+<input type="radio" name="designation"> Project Lead
+</td>
+</tr>
+<tr>
+ <td>   Preferred language</td>
+<td> 
+    <input type="checkbox" name="preferred language"> JAVA
+    <input type="checkbox" name="preferred language"> PHP
+    <input type="checkbox" name="preferred language"> C++ 
+</td>
+</tr>
+<tr>
+ <td> E-mail: </td>   
+</tr>
+<tr>
+ <td> Password: </td>   
+</tr>
+<tr> 
+ <td> Please choose a File: </td> 
+  </tr>
+  </table>
+  <input type="submit" value="Choose File">
+  <input type="submit" value=" No File Choose">
+  <input type="submit" value="Submit">
+  <input type="reset" value="reset">
+</from>
