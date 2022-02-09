6146b22612547a6e2dcded0e817179776f058655
BadhanAkter <98724609+BadhanAkter@users.noreply.github.com>
2022-02-09 11:18:38 +0600

    Add files via upload

---
+++ Task2.php/result.php U.php
@@ -0,0 +1,9 @@
+<?php
+$fname = $lname = $age = $designation = $prefeered language = $email = $pass = $choose a file = " ";
+if ($_SERVER["REQUEST_METHOD"]=="POST"){
+    $fname=
+    test_input($_POST["fname"]);
+    $lname=
+    test_input($_POST["lname"]);
+    $age=designation
+}
