700ebcecde10725ad7b2496fc23a219b2ae760ae
BadhanAkter <98724609+BadhanAkter@users.noreply.github.com>
2022-02-16 19:22:58 +0600

    Create form.php

---
+++ TASK3/form.php
@@ -0,0 +1,57 @@
+# WT_Spring2022
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1> Registration Form </h1>
+<form action="/action_page.php" method="POST">
+    
+
+ First Name: <br>
+ <input type="text" first name="first name" placeholder="Enter your first name"> 
+    <br></br>
+
+ Last Name: <br>
+ <input type="text" last name="last name" placeholder="Enter your last name">
+    <br></br>  
+
+
+  Age: <br>
+ <input type="number">   
+ <br></br>
+
+ Designation:<br>
+
+<input type="radio" name="designation"> Junior Programmer
+<input type="radio" name="designation"> Senior Programmer
+<input type="radio" name="designation"> Project Lead
+<br></br>
+
+
+   Preferred language<br>
+ 
+    <input type="checkbox" name="preferred language"> JAVA
+    <input type="checkbox" name="preferred language"> PHP
+    <input type="checkbox" name="preferred language"> C++ 
+    <br></br>
+
+  E-mail: <br>  
+  <input type="text" e-mail="e-mail"> 
+  <br></br> 
+
+  Password: <br>
+ <input type="text" password="password">   
+ <br></br> 
+
+ Please choose a File: <br>
+  <input type="submit" value="choose File"> 
+   No Choose File 
+   <br></br>
+ 
+  
+  <input type="submit" value="Submit">
+  <input type="reset" value="reset">
+  <br></br>
+</from>
+</body>
+</html>
